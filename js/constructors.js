0cbd6bfe0652900d68c5b00bc1f3c9593f0a2337
Tanesha Brester <taneshabrester@gmail.com>
2022-03-01 17:50:58 -0800

    finished terms definitions

--- js/constructors.js
+++ js/constructors.js
@@ -46,8 +46,10 @@ new Word('parse','Parsing means analyzing and converting a program into an inte
 
 new Word('color','Colors are typically expressed through hexadecimal codes, either prefixed using a pound sign (#) or 0x to denote base 16 values');
 
-new Word('lists',);
-new Word('links',);
+new Word('lists','');
+
+new Word('links','A link (short for hyperlink) is an HTML object that allows you to jump to a new location when you click or tap it. Links are found on almost every webpage and provide a simple means of navigating between pages on the web. Links can be attached to text, images, or other HTML elements');
+
 new Word('class','Classes are a template for creating objects. They encapsulate data with code to work on that data. Classes in JS are built on prototypes but also have some syntax and semantics that are not shared with ES5 class-like semantics.');
 
 new Word('block','The block statement is often called compound statement in other languages. It allows you to use multiple statements where JavaScript expects only one statement. Combining statements into blocks is a common practice in JavaScript.');
@@ -56,7 +58,10 @@ new Word('image', 'The image object represents the HTML element.');
 
 new Word('forms','When the page is loaded, JavaScript makes an array forms in which it puts all the forms that are on the page. The first form is forms[0] , the second is forms[1] etc. Each form has another array in which JavaScript puts all the elements in the form. The first elements is elements[0] , the second elements[1] etc');
 
-new Word('fonts',);
-new Word('icons',);
-new Word('align',);
-new Word('units',);
+new Word('fonts','Any design of type, including a full range of characters, as letters, numbers, and marks of punctuation, in all sizes. c. Also called typeface. the general style or appearance of type: broad or narrow face.');
+
+new Word('icons','An icon is a glyph used to represent something else.');
+
+new Word('align','The align attribute specifies the alignment of an <object> element according to the surrounding element. The <object> element is an inline element (it does not insert a new line on a page), meaning that text and other elements can wrap around it.');
+
+new Word('units','Unit.js is an assertion library for Javascript, running on Node.js and the browser. It works with any test runner and unit testing framework');
