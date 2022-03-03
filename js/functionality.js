c72b51eedbf3d3c3c44247db8cb848ef95fca696
Cole Gibbs <colegibbs0@gmail.com>
2022-03-03 11:08:49 -0800

    displayResults function

--- js/functionality.js
+++ js/functionality.js
@@ -23,7 +23,7 @@ instantiate new results object to increment on game win or lose.
 //DOM window for winOrLose
 let endGameAlert = document.getElementById('alert-container');
 
-//local storage use
+// local storage use
 // let parsedResults = JSON.parse(localStorage.getItem('storedResults'));
 // let results;
 
@@ -165,6 +165,28 @@ function winOrLose(results, word) {
   resultsButton.textContent = 'Results';
   aElem.appendChild(resultsButton);
   endGameAlert.appendChild(aElem);
+}
+
+function resultsDisplay(results) {
+  let totalRounds = document.getElementById('rounds-played');
+  let pElem = document.createElement('p');
+  pElem.textContent = results.roundsPlayed;
+  totalRounds.appendChild(pElem);
+
+  let winPercentage = document.getElementById('win-percentage');
+  let p1Elem = document.createElement('p');
+  p1Elem.textContent = results.winPercent;
+  winPercentage.appendChild(p1Elem);
+
+  let currentWins = document.getElementById('win-streak');
+  let p2Elem = document.createElement('p');
+  p2Elem.textContent = results.currentStreak;
+  currentWins.appendChild(p2Elem);
+
+  let bestWins = document.getElementById('best-win-streak');
+  let p3Elem = document.createElement('p');
+  p3Elem.textContent = results.bestStreak;
+  bestWins.appendChild(p3Elem);
 }
 
 // ------------ EVENT HANDLERS -------------
